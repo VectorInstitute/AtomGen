2c1c48fa47754cb40ca61a950e6fcfe9ec524e14
akore <akore0x5f@gmail.com>
2024-06-26 18:29:27 -0400

    fix s2ef pretrain script

--- scripts/training/pretrain_s2ef.py
+++ scripts/training/pretrain_s2ef.py
@@ -27,7 +27,7 @@ def parse_arguments():
     parser.add_argument("--micro_batch_size", type=int, default=8, help="Micro batch size")
     parser.add_argument("--macro_batch_size", type=int, default=128, help="Macro batch size")
     parser.add_argument("--max_steps", type=int, default=-1, help="Maximum number of training steps")
-    parser.add_argument("--gradient_checkpointing", type=bool, default=False, help="Whether to use gradient checkpointing")
+    parser.add_argument("--gradient_checkpointing", action=argparse.BooleanOptionalAction, help="Whether to use gradient checkpointing")
     parser.add_argument("--num_train_epochs", type=float, default=10, help="Number of training epochs")
     parser.add_argument("--warmup_ratio", type=float, default=0.001, help="Warmup ratio for learning rate scheduler")
     parser.add_argument("--lr_scheduler_type", type=str, default="cosine", help="Type of learning rate scheduler")
@@ -38,11 +38,11 @@ def parse_arguments():
     parser.add_argument("--log_steps", type=int, default=150, help="Number of steps to log the training")
     parser.add_argument("--learning_rate", type=float, default=3e-4, help="Learning rate")
     parser.add_argument("--num_cpus", type=int, default=-1, help="Number of cpus available")
-    parser.add_argument("--fp16", type=bool, default=True, help="Whether to use fp16 precision")
     parser.add_argument("--save_total_limit", type=int, default=1, help="Total number of checkpoints to keep")
     parser.add_argument("--dataloader_num_workers", type=int, default=-1, help="Number of dataloader workers")
-    parser.add_argument("--group_by_length", type=bool, default=False, help="Whether to group samples by length")
+    parser.add_argument("--group_by_length", action=argparse.BooleanOptionalAction, help="Whether to group samples by length")
     parser.add_argument("--length_column_name", type=str, default="length", help="Name of the length column")
+    parser.add_argument('--checkpoint-exists', action=argparse.BooleanOptionalAction)
     return parser.parse_args()
 
 def train(args):
@@ -59,7 +59,7 @@ def train(args):
         print(f"PRE-TRAINED WEIGHT LOADED FROM {args.weights_dir}")
     else:
         config = AtomformerConfig.from_json_file(args.model_config)
-        config.gradient_checkpointing = args.gradient_checkpointing
+        config.gradient_checkpointing = args.gradient_checkpointing if args.gradient_checkpointing else False
         model = Structure2EnergyAndForces(config)
 
     tokenizer = AtomTokenizer(vocab_file=args.tokenizer_json)
@@ -83,7 +83,6 @@ def train(args):
         per_device_train_batch_size=args.micro_batch_size,
         eval_accumulation_steps=args.eval_accum_steps//(args.macro_batch_size//args.micro_batch_size),
         gradient_accumulation_steps=args.macro_batch_size//args.micro_batch_size,
-        fp16=args.fp16,
         label_names=["formation_energy", "forces", "total_energy"],
         save_total_limit=args.save_total_limit,
         logging_steps=args.log_steps//(args.macro_batch_size//args.micro_batch_size),
@@ -103,29 +102,7 @@ def train(args):
         data_collator=data_collator,
     )
 
-    def checkpoint_exists(output_dir, local_rank):
-        if local_rank == 0:
-            for root, dirs, files in os.walk(output_dir, topdown=False):
-                for name in files:
-                    if name == "model.safetensors":
-                        return True
-            for root, dirs, files in os.walk(f"/checkpoint/{user}/", topdown=False):
-                for dir in dirs:
-                    if dir == args.name:
-                        for root2, dirs, files in os.walk(os.path.join(root, dir), topdown=False):
-                            for dir in dirs:
-                                if dir.startswith('checkpoint'):
-                                    os.system(f"cp -r {os.path.join(root, root2, dir)} {output_dir}")
-                                    os.system(f"rm -rf {os.path.join(root, root2, dir)}")
-                                    return True
-        else:
-            for root, dirs, files in os.walk(output_dir, topdown=False):
-                for name in files:
-                    if name == "model.safetensors":
-                        return True
-        return False
-
-    trainer.train(resume_from_checkpoint=checkpoint_exists(args.output_dir, local_rank))
+    trainer.train(resume_from_checkpoint=args.checkpoint_exists)
 
     model.save_pretrained(args.output_dir)
 
