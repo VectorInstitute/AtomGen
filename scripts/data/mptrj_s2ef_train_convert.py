8c3961e407f96d05052c83b4046e53ef38c5b774
a-kore <37000693+a-kore@users.noreply.github.com>
2024-07-03 09:50:38 -0400

    Update scripts (#25)
    * adjust configs and add scripts
    
    * add training scripts
    
    * fix s2ef pretrain script
    
    * [pre-commit.ci] Add auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * remove scratch notebook
    
    * remove scratch notebook
    
    * fix mypy errors
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ scripts/data/mptrj_s2ef_train_convert.py
@@ -0,0 +1,146 @@
+"""Scripts to pre-process MPtrj dataset into a HuggingFace dataset."""
+
+import argparse
+import json
+from typing import Any, Dict
+
+import numpy as np
+from datasets import Dataset
+from pymatgen.core import Structure
+from tqdm import tqdm
+
+
+def process_json_chunk(chunk: str) -> Dict[str, Any]:
+    """Process a chunk of JSON data and return a dictionary with the processed data."""
+    json_loaded = json.loads(chunk)
+    struct = Structure.from_dict(json_loaded["structure"])
+
+    input_ids = np.array(list(struct.atomic_numbers)).astype("int16")
+    return {
+        "input_ids": input_ids,
+        "coords": struct.cart_coords.astype("float32"),
+        "forces": np.array(json_loaded["force"]).astype("float32"),
+        "formation_energy": np.array(
+            json_loaded["ef_per_atom"] * len(input_ids)
+        ).astype("float32"),
+        "total_energy": np.array(json_loaded["corrected_total_energy"]).astype(
+            "float32"
+        ),
+        "has_formation_energy": True,
+    }
+
+
+def main(args: argparse.Namespace) -> None:
+    """Process the MPtrj dataset."""
+    with open(args.input_file, "r") as file:
+        output = ""
+        dataset: Dict[str, Any] = {
+            "input_ids": [],
+            "coords": [],
+            "forces": [],
+            "formation_energy": [],
+            "total_energy": [],
+            "has_formation_energy": [],
+        }
+        num_datasets = 0
+        num_samples = 0
+        pbar = tqdm(total=args.total_samples)
+        read = True
+
+        while True:
+            try:
+                if read:
+                    output = output + file.read(int(1e6))
+                    read = False
+                start = output.find('{"structure"')
+                end = output.find('"mp_id"')
+                if start != -1 and end != -1:
+                    end = output.find("}", end)
+                    if end != -1:
+                        end += 1
+                        num_samples += 1
+                        pbar.update(1)
+
+                        chunk_data = process_json_chunk(output[start:end])
+                        for key, value in chunk_data.items():
+                            dataset[key].append(value)
+
+                        output = output[end:]
+
+                        if num_samples == args.samples_per_dataset:
+                            dataset_obj = Dataset.from_dict(dataset)
+                            dataset_obj.save_to_disk(
+                                f"{args.output_dir}/{num_datasets}"
+                            )
+                            num_datasets += 1
+                            num_samples = 0
+                            dataset = {
+                                "input_ids": [],
+                                "coords": [],
+                                "forces": [],
+                                "formation_energy": [],
+                                "total_energy": [],
+                                "has_formation_energy": [],
+                            }
+                            pbar.close()
+                            pbar = tqdm(total=args.total_samples)
+                    else:
+                        read = True
+                        continue
+                else:
+                    read = True
+                    continue
+            except Exception as e:
+                print(f"An error occurred: {e}")
+                break
+
+        pbar.close()
+
+        # Save any remaining data
+        if num_samples > 0:
+            dataset_obj = Dataset.from_dict(dataset)
+            dataset_obj.save_to_disk(f"{args.output_dir}/{num_datasets}")
+
+        # Concatenate all datasets
+        all_datasets = [
+            Dataset.load_from_disk(f"{args.output_dir}/{i}")
+            for i in range(num_datasets + 1)
+        ]
+        final_dataset = Dataset.concatenate_datasets(all_datasets)
+        final_dataset.save_to_disk(args.final_output)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Preprocess MPtrj dataset into a HuggingFace dataset."
+    )
+    parser.add_argument(
+        "--input_file", type=str, required=True, help="Path to the input JSON file"
+    )
+    parser.add_argument(
+        "--output_dir",
+        type=str,
+        required=True,
+        help="Directory to save processed datasets",
+    )
+    parser.add_argument(
+        "--final_output",
+        type=str,
+        required=True,
+        help="Path to save the final concatenated dataset",
+    )
+    parser.add_argument(
+        "--total_samples",
+        type=int,
+        default=1580395,
+        help="Total number of samples in the dataset",
+    )
+    parser.add_argument(
+        "--samples_per_dataset",
+        type=int,
+        default=1580394,
+        help="Number of samples per dataset chunk",
+    )
+
+    args = parser.parse_args()
+    main(args)
---
+++ scripts/data/oc20_s2ef_train_convert.py
@@ -0,0 +1,135 @@
+"""Scripts to pre-process OC20 dataset into a HuggingFace dataset."""
+
+import argparse
+import glob
+import lzma
+import os
+from multiprocessing import Pool
+from typing import Any, List, Tuple
+
+import pandas as pd
+from ase import io
+from datasets import Dataset, concatenate_datasets, load_from_disk
+from tqdm import tqdm
+
+
+def extract_xz(xz_path: str) -> str:
+    """Extract a .xz file and returns the path to the extracted file."""
+    output_path = xz_path.replace(".xz", "")
+    with lzma.open(xz_path) as f, open(output_path, "wb") as out:
+        file_content = f.read()
+        out.write(file_content)
+    return output_path
+
+
+def extract_txt(txt_path: str) -> str:
+    """Extract a .txt file and returns the path to the extracted file."""
+    output_path = txt_path.replace(".xz", "")
+    with lzma.open(txt_path) as f, open(output_path, "wb") as out:
+        file_content = f.read()
+        out.write(file_content)
+    return output_path
+
+
+def process_data(xz_txt_paths: List[Tuple[str, str, str]]) -> None:
+    """Process a chunk of data."""
+    for xz_txt_path in xz_txt_paths:
+        dataset_dict: dict[str, List[Any]] = {
+            "input_ids": [],
+            "coords": [],
+            "forces": [],
+            "formation_energy": [],
+            "total_energy": [],
+            "has_formation_energy": [],
+        }
+        xz_path, txt_path, output_dir = xz_txt_path
+        atoms_path = extract_xz(xz_path)
+        energy_path = extract_txt(txt_path)
+        meta = pd.read_csv(
+            energy_path, names=["system_id", "frame_id", "reference_energy"]
+        )
+        atoms = io.read(atoms_path, index=":")
+        os.remove(atoms_path)
+        os.remove(energy_path)
+
+        for i in range(len(atoms)):
+            dataset_dict["input_ids"].append(
+                atoms[i].get_atomic_numbers().astype("int16")
+            )
+            dataset_dict["coords"].append(atoms[i].get_positions().astype("float32"))
+            dataset_dict["forces"].append(
+                atoms[i].get_forces(apply_constraint=False).astype("float32")
+            )
+            dataset_dict["formation_energy"].append(
+                atoms[i].get_potential_energy(apply_constraint=False).astype("float32")
+                - meta.iloc[i]["reference_energy"].astype("float32")
+            )
+            dataset_dict["total_energy"].append(
+                atoms[i].get_potential_energy(apply_constraint=False).astype("float32")
+            )
+            dataset_dict["has_formation_energy"].append(True)
+
+        dataset = Dataset.from_dict(dataset_dict)
+        dataset.save_to_disk(
+            os.path.join(output_dir, f'{xz_path.split("/")[-1].split(".")[0]}')
+        )
+
+
+def main() -> None:
+    """Process the OC20 S2EF dataset."""
+    parser = argparse.ArgumentParser(description="Process OC20 S2EF data")
+    parser.add_argument(
+        "--split", type=str, help='The value to replace "val_id" in the paths'
+    )
+    parser.add_argument(
+        "--output_dir",
+        type=str,
+        default="/mnt/data2/atomistic_datasets/opencatalyst/s2ef/oc20_s2ef",
+        help="Output directory",
+    )
+    parser.add_argument(
+        "--num_processes", type=int, default=11, help="Number of processes to use"
+    )
+    args = parser.parse_args()
+
+    output_dir = args.output_dir + f"_{args.split}" + "_datasets"
+
+    oc20_s2ef_xz = glob.glob(
+        f"/mnt/data2/atomistic_datasets/opencatalyst/s2ef/oc20_s2ef-val-test/{args.split}/s2ef_{args.split}/*.extxyz.xz"
+    )
+    oc20_s2ef_xz = sorted(
+        oc20_s2ef_xz, key=lambda x: int(x.split("/")[-1].split(".")[0])
+    )
+
+    oc20_s2ef_txt = glob.glob(
+        f"/mnt/data2/atomistic_datasets/opencatalyst/s2ef/oc20_s2ef-val-test/{args.split}/s2ef_{args.split}/*.txt.xz"
+    )
+    oc20_s2ef_txt = sorted(
+        oc20_s2ef_txt, key=lambda x: int(x.split("/")[-1].split(".")[0])
+    )
+
+    # Split data into chunks for parallel processing
+    chunks = list(zip(oc20_s2ef_xz, oc20_s2ef_txt, [output_dir] * len(oc20_s2ef_xz)))
+    chunk_size = len(chunks) // args.num_processes
+    chunks = [chunks[i : i + chunk_size] for i in range(0, len(chunks), chunk_size)]  # type: ignore
+
+    # Create a pool of processes
+    with Pool(args.num_processes) as pool:
+        for _ in tqdm(pool.imap_unordered(process_data, chunks), total=len(chunks)):  # type: ignore
+            pass
+
+    datasets = []
+    oc20_s2ef_paths = glob.glob(output_dir + "/*")
+    oc20_s2ef_paths = sorted(oc20_s2ef_paths, key=lambda x: int(x.split("/")[-1]))
+    # load and concatenate the datasets
+    for path in tqdm(oc20_s2ef_paths):
+        datasets.append(load_from_disk(path))
+
+    oc20_s2ef = concatenate_datasets(datasets)
+    oc20_s2ef.save_to_disk(output_dir[:-9])
+
+    os.system(f"rm -rf {output_dir}")
+
+
+if __name__ == "__main__":
+    main()
